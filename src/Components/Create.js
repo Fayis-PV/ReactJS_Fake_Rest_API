64f8fef3450c73a9c0526bbb7d36c87b2d6d0004
Muhammed Fayis P V <fayispvchelari@gmail.com>
2025-02-16 11:01:11 +0530

    Updated: Create Form credentials and submit

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -23,46 +24,54 @@ const Create = () => {
         }
       })
     }
-    console.log(formData);
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log(formData);
+      
+      axios.post('https://jsonplaceholder.typicode.com/users/',
+        formData,
+      )
+      .then((response)=>{
+        if (response.status === 201){
+          console.log('User Successfully Created')
+        }
+      })
+      .catch((error) => console.log(error))
+    }
     
   return (
     <div>
       <h1>Create Page</h1>
       <Link to={'/'} className='my-2 btn btn-primary float-right'>Home</Link>
-      <form method="post" className='row'>
-        <div className='form-group col-sm-6 mt-2'>
-            <label for="name" className='form-label'>Name: </label>
+      <form method="post" className='row' onSubmit={handleSubmit}>
+        <div className='form-group mt-2'>
+            <label htmlFor="name" className='form-label'>Name: </label>
             <input type="text" name="name" id="name" className='form-control' placeholder='Enter Name' onChange={handleChange} />
         </div>
         <div className='form-group col-sm-6 mt-2'>
-            <label for="name" className='form-label'>Username: </label>
-            <input type="text" name="username" id="username" className='form-control' placeholder='Enter Userame' onChange={handleChange} />
-        </div>
-        <div className='form-group col-sm-6 mt-2'>
-            <label for="name" className='form-label'>Email: </label>
+            <label htmlFor="name" className='form-label'>Email: </label>
             <input type="email" name="email" id="email" className='form-control' placeholder='Enter Email' onChange={handleChange} />
         </div>
         <div className='form-group col-sm-6 mt-2'>
-            <label for="name" className='form-label'>Phone: </label>
+            <label htmlFor="name" className='form-label'>Phone: </label>
             <input type="text" name="phone" id="phone" className='form-control' placeholder='Enter Phone' onChange={handleChange} />
         </div>
-        <div className='form-group col-sm-6 mt-2'>
-            <label for="name" className='form-label'>Address: </label>
-            <input type="text" name="street" id="street" className='form-control' placeholder='Enter Street' onChange={handleAddressChange} />
-        </div>
-        <div className='form-group col-sm-6 mt-2' style={{paddingTop:'32px'}}>
-            <input type="text" name="suite" id="suite" className='form-control' placeholder='Enter Suite' onChange={handleAddressChange} />
-        </div>
-        <div className='form-group col-sm-6 mt-2'>
-            <input type="text" name="city" id="city" className='form-control' placeholder='Enter City' onChange={handleAddressChange} />
-        </div>
-        <div className='form-group col-sm-6 mt-2'>
-            <input type="text" name="zipcode" id="zipcode" className='form-control' placeholder='Enter Zipcode' onChange={handleAddressChange} />
-        </div>
-        <div className='form-group col-sm-6 mt-2'>
-        <label for="name" className='form-label'>Website: </label>
-            <input type="text" name="website" id="website" className='form-control' placeholder='Enter Website' onChange={handleChange} />
-        </div>
+        <div className='form-group mt-2 row'>
+            <label htmlFor="name" className='form-label'>Address: </label>
+            <div className='col-sm-6'>
+              <input type="text" name="street" id="street" className='form-control' placeholder='Enter Street' onChange={handleAddressChange} />
+            </div>
+            <div className='col-sm-6'>
+              <input type="text" name="suite" id="suite" className='form-control' placeholder='Enter Suite' onChange={handleAddressChange} />
+            </div>
+            <div className='col-sm-6 mt-2'>
+              <input type="text" name="city" id="city" className='form-control' placeholder='Enter City' onChange={handleAddressChange} />
+            </div>
+            <div className='col-sm-6 mt-2'>
+              <input type="text" name="zipcode" id="zipcode" className='form-control' placeholder='Enter Zipcode' onChange={handleAddressChange} />
+            </div>
+        </div>  
         <button type='submit' className='btn btn-success my-3 py-2 px-4'>Create</button>
       </form>
     </div>
